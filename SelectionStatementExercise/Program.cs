2e245945e2271aa532f3e8727356717830c34a88
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-21 14:13:10 -0500

    excercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            Console.WriteLine("Try to guess my favorite number");
+            var favnum = int.Parse(Console.ReadLine());
+
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+
+
+            if (favnum < favNumber)
+
+                Console.WriteLine("too low");
+
+            else if (favnum > favNumber)
+
+                Console.WriteLine(" too high");
+
+            else
+            {
+                Console.WriteLine("You guess it");
+                    
+            }
+
+
+
+
+
         }
     }
 }
